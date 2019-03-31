1b83f79243fa68bd42fce1c883fa83eef99d1982
Kavin Phan <phan.kavin@gmail.com>
2019-03-31 02:22:51 -0400

    add user functions

--- fb_util.py
+++ fb_util.py
@@ -4,9 +4,35 @@ from firebase_admin import firestore
 from pathlib import Path
 
 db = None
+users_coll = None
+
+class User:
+	def __init__(self, campus_id, f_name, l_name, points):
+		self.campus_id = campus_id
+		self.f_name = f_name
+		self.l_name = l_name
+		self.points = points
+	
+	@staticmethod
+	def from_dict(src):
+		return User(
+			src["campus_id"], src["f_name"], src["l_name"], src["points"]
+		)
+	
+	def to_dict(self):
+		return {
+			"campus_id": self.campus_id, "f_name": self.f_name, "l_name": self.l_name, "points": self.points
+		}
+
+	def __repr__(self):
+		return(
+			"User(campus_id={}, f_name={}, l_name={}, points={})".format(
+				self.campus_id, self.f_name, self.l_name, self.points
+			)
+		)
 
 def init():
-	global db
+	global db, users_coll
 
 	base_path = Path(__file__).parent
 	key_path = (base_path / "./security/ecodrop-key.json").resolve()
@@ -16,3 +42,19 @@ def init():
 	firebase_admin.initialize_app(cred)
 
 	db = firestore.client()
+	users_coll = db.collection("users")
+
+# TODO add try-catch
+def get_user(iso):
+	user_doc_ref = users_coll.document(iso)
+	user_doc = user_doc_ref.get()
+	user_obj = User.from_dict(user_doc.to_dict())
+
+	return user_obj
+
+def add_points_iso(iso, user, points):
+	user_dict = user.to_dict()
+	user_doc_ref = users_coll.document(iso)
+
+	user_dict["points"] += points
+	user_doc_ref.set(user_dict)
