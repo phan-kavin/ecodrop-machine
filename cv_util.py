b543571697e9265523c3a4149be3fb86e56eb81b
Kavin Phan <phan.kavin@gmail.com>
2019-03-31 02:35:34 -0400

    add option for "asap" frame processing

--- cv_util.py
+++ cv_util.py
@@ -38,7 +38,7 @@ def begin_scanning(timeout, iso):
 		if current_time - start_time >= timeout:
 			break
 
-		if current_time - last_frame_time >= time_skip:
+		if time_skip < 0 or current_time - last_frame_time >= time_skip:
 			last_frame_time = current_time
 
 			process_frame(iso)
